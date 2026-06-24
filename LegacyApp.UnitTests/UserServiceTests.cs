23105396598d69a30d3e4cda8cfa585ee5c604ef
Marcelo Amaral <msantana.amaral@hotmail.com>
2026-06-24 17:25:41 -0300

    Add a test class to cover the behavior of the UserService class. The initial test covers a happy path of the AddUser method.

---
+++ LegacyApp.UnitTests/UserServiceTests.cs
@@ -0,0 +1,23 @@
+﻿namespace LegacyApp.UnitTests;
+
+public class UserServiceTests
+{
+    public UserServiceTests()
+    {
+
+    }
+
+    [Fact]
+    public void AddUser_ShouldReturnTrue_WhenUserIsSaved()
+    {
+        // Arrange
+        var sut = new UserService(x => { }, id => new Client { Id = id, Name = "VeryImportantClient" });
+        // Act
+        var result = sut.AddUser("John", "Doe", "john.doe@example.com", new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Local), 1);
+        // Assert
+        Assert.True(result);
+    }
+
+
+
+}
