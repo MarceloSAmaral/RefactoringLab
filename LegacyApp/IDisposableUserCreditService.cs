71035ff59cc9165d0bacb219e5c660c9870ee2a0
Marcelo Amaral <msantana.amaral@hotmail.com>
2026-06-25 12:13:19 -0300

    Create the IDisposableUserCreditService interface to bind together the IUserCreditService and IDisposable interfaces.

---
+++ LegacyApp/IDisposableUserCreditService.cs
@@ -0,0 +1,10 @@
+﻿using LegacyApp;
+using System;
+
+/// <summary>
+/// This interface binds together the IUserCreditService and IDisposable interfaces.
+/// </summary>
+public interface IDisposableUserCreditService : IUserCreditService, IDisposable
+{
+
+}
