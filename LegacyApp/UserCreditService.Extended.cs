68f721acc2242f73cec6397a3ca268fdbfc7997f
Marcelo Amaral <msantana.amaral@hotmail.com>
2026-06-25 12:17:28 -0300

    Extend UserCreditServiceClient type declaration with a new file, the UserCreditService.Extended.cs.

---
+++ LegacyApp/UserCreditService.Extended.cs
@@ -0,0 +1,14 @@
+﻿namespace LegacyApp
+{
+
+    public partial class UserCreditServiceClient : IDisposableUserCreditService
+    {
+
+        /*
+            We should not change the UserCreditService.cs file because it is generated code. 
+            Instead, we can create a new partial class file to extend the type declaration and 
+            add the IDisposableUserCreditService interface.
+        */
+
+    }
+}
