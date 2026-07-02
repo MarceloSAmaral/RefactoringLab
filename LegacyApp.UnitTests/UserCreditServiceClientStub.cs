8b437ed33c7b960548b58ba7d11469510036b329
Marcelo Amaral <msantana.amaral@hotmail.com>
2026-07-02 08:31:02 -0300

    Expanding the unit tests to cover more of the rules and behaviior from the UserService class.

---
+++ LegacyApp.UnitTests/UserCreditServiceClientStub.cs
@@ -0,0 +1,18 @@
+﻿namespace LegacyApp.UnitTests;
+
+class UserCreditServiceClientStub : IDisposableUserCreditService
+{
+    private readonly Func<int> _GetCreditLimitImplementation;
+    public UserCreditServiceClientStub(Func<int> getCreditLimitImplementation)
+    {
+        _GetCreditLimitImplementation = getCreditLimitImplementation;
+    }
+
+    public int GetCreditLimit(string firstname, string surname, DateTime dateOfBirth)
+    {
+        return _GetCreditLimitImplementation();
+    }
+
+    public void Dispose() { }
+
+}
--- LegacyApp.UnitTests/UserServiceTests.cs
+++ LegacyApp.UnitTests/UserServiceTests.cs
@@ -1,7 +1,18 @@
-﻿namespace LegacyApp.UnitTests;
+﻿using Moq;
+
+namespace LegacyApp.UnitTests;
 
 public class UserServiceTests
 {
+
+    const string FirstName = "John";
+    const string LastName = "Doe";
+    const string Email = "john.doe@example.com";
+    const int ClientId = 1;
+    readonly DateTime userDateOfBirth = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Local);
+    const int EnoughCreditLimit = 500;
+
+
     public UserServiceTests()
     {
 
@@ -13,11 +24,123 @@ public class UserServiceTests
         // Arrange
         var sut = new UserService(x => { }, id => new Client { Id = id, Name = "VeryImportantClient" }, () => new UserCreditServiceClient());
         // Act
-        var result = sut.AddUser("John", "Doe", "john.doe@example.com", new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Local), 1);
+        var result = sut.AddUser(FirstName, LastName, Email, userDateOfBirth, ClientId);
         // Assert
         Assert.True(result);
     }
 
+    [Fact(DisplayName = "AddUser should mark client with HasCreditLimit as false when a VeryImportantClient client's user is saved")]
+    public void AddUser_ShouldMarkHasCreditLimitAsFalse_WhenAVeryImportantClientUserIsSaved()
+    {
+        Mock<Action<User>> mockAddUserMethod = new Mock<Action<User>>(MockBehavior.Strict);
+        mockAddUserMethod
+            .Setup(addUser => addUser(It.Is<User>(user => user.HasCreditLimit == false)))
+            .Verifiable();
+        var sut = new UserService(mockAddUserMethod.Object, id => new Client { Id = id, Name = "VeryImportantClient" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
 
+        var result = sut.AddUser(FirstName, LastName, Email, userDateOfBirth, ClientId);
 
+        Assert.True(result);
+        mockAddUserMethod.Verify(addUser => addUser(It.Is<User>(user => user.HasCreditLimit == false)), Times.Once);
+    }
+
+    [Fact(DisplayName = "AddUser should mark client with HasCreditLimit as true and it should be the double of what the CreditService reports when an ImportantClient client's user is saved")]
+    public void AddUser_ShouldMarkHasCreditLimitAsTrueAndItShouldBeTheDoubleOfWhatCreditServiceReports_WhenAnImportantClientUserIsSaved()
+    {
+        Mock<Action<User>> mockAddUserMethod = new Mock<Action<User>>(MockBehavior.Strict);
+        mockAddUserMethod
+            .Setup(addUser => addUser(It.Is<User>(user => user.HasCreditLimit == true && user.CreditLimit == EnoughCreditLimit * 2)))
+            .Verifiable();
+        var sut = new UserService(mockAddUserMethod.Object, id => new Client { Id = id, Name = "ImportantClient" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(FirstName, LastName, Email, userDateOfBirth, ClientId);
+
+        Assert.True(result);
+        mockAddUserMethod.Verify(addUser => addUser(It.Is<User>(user => user.HasCreditLimit == true && user.CreditLimit == EnoughCreditLimit * 2)), Times.Once);
+    }
+
+    [Fact(DisplayName = "AddUser should mark client with HasCreditLimit as true and it should be what the CreditService reports when a regular client's user is saved")]
+    public void AddUser_ShouldMarkHasCreditLimitAsTrueAndItShouldBeTheWhatCreditServiceReports_WhenAVeryImportantClientUserIsSaved()
+    {
+        Mock<Action<User>> mockAddUserMethod = new Mock<Action<User>>(MockBehavior.Strict);
+        mockAddUserMethod
+            .Setup(addUser => addUser(It.Is<User>(user => user.HasCreditLimit == true)))
+            .Verifiable();
+        var sut = new UserService(mockAddUserMethod.Object, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(FirstName, LastName, Email, userDateOfBirth, ClientId);
+
+        Assert.True(result);
+        mockAddUserMethod.Verify(addUser => addUser(It.Is<User>(user => user.HasCreditLimit == true && user.CreditLimit == EnoughCreditLimit)), Times.Once);
+    }
+
+    [Fact(DisplayName = "AddUser should deny saving a user when they have CreditLimit and it is below 500")]
+    public void AddUser_ShouldDenySavingAClientsUser_WhenUserHasCreditidLimitAndItIsBelow500()
+    {
+        var sut = new UserService(x => { throw new InvalidOperationException("User saving denied"); }, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => 100));
+
+        var result = sut.AddUser(FirstName, LastName, Email, userDateOfBirth, ClientId);
+
+        Assert.False(result);
+    }
+
+    [Fact(DisplayName = "AddUser should deny saving a user when the firstname is empty")]
+    public void AddUser_ShouldDenySavingAClientsUser_WhenFirstNameIsEmpty()
+    {
+        var sut = new UserService(x => { throw new InvalidOperationException("User saving denied"); }, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(String.Empty, LastName, Email, userDateOfBirth, ClientId);
+
+        Assert.False(result);
+    }
+
+    [Fact(DisplayName = "AddUser should deny saving a user when the firstname is null")]
+    public void AddUser_ShouldDenySavingAClientsUser_WhenFirstNameIsNull()
+    {
+        var sut = new UserService(x => { throw new InvalidOperationException("User saving denied"); }, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(null, LastName, Email, userDateOfBirth, ClientId);
+
+        Assert.False(result);
+    }
+
+    [Fact(DisplayName = "AddUser should deny saving a user when the lastname is empty")]
+    public void AddUser_ShouldDenySavingAClientsUser_WhenLastNameIsEmpty()
+    {
+        var sut = new UserService(x => { throw new InvalidOperationException("User saving denied"); }, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(FirstName, String.Empty, Email, userDateOfBirth, ClientId);
+
+        Assert.False(result);
+    }
+
+    [Fact(DisplayName = "AddUser should deny saving a user when the lastname is null")]
+    public void AddUser_ShouldDenySavingAClientsUser_WhenLastNameIsNull()
+    {
+        var sut = new UserService(x => { throw new InvalidOperationException("User saving denied"); }, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(FirstName, null, Email, userDateOfBirth, ClientId);
+
+        Assert.False(result);
+    }
+
+    [Fact(DisplayName = "AddUser should deny saving a user when the email address has a @ symbol but no dot")]
+    public void AddUser_ShouldDenySavingAClientsUser_WhenEmailHasAtSymbolButNoDot()
+    {
+        var sut = new UserService(x => { throw new InvalidOperationException("User saving denied"); }, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(FirstName, LastName, "invalid@email", userDateOfBirth, ClientId);
+
+        Assert.False(result);
+    }
+
+    [Fact(DisplayName = "AddUser should deny saving a user when the age is below 21")]
+    public void AddUser_ShouldDenySavingAClientsUser_WhenAgeIsBelow21()
+    {
+        var sut = new UserService(x => { throw new InvalidOperationException("User saving denied"); }, id => new Client { Id = id, Name = "Regular Client" }, () => new UserCreditServiceClientStub(() => EnoughCreditLimit));
+
+        var result = sut.AddUser(FirstName, LastName, Email, System.DateTime.Now.Date.AddYears(-20), ClientId);
+
+        Assert.False(result);
+    }
 }
